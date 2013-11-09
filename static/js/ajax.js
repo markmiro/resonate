8d3aca3c964962f7a88b4c27c940d42d08191fe8
Yaadhav Raaj <raaj@lark.com>
2013-11-09 15:18:39 -0800

    added postCards API

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -26,3 +26,13 @@ function postUserBio(userid, biojson, callback){
     })
     .fail(function() { console.log("postUserBio Error"); })
 }
+
+//[{"userid":"1","header":"Experiences","bullets":[{"bullet":"CEO of Apples","date":null,"image":null},{"bullet":"Leader of a pack","date":null,"image":null},{"bullet":"Stud","date":null,"image":null}]},{"userid":"1","header":"Power","bullets":[{"bullet":"Boss","date":null,"image":null},{"bullet":"Damn Son..","date":null,"image":null}]}]
+function postCards(userid, cards, callback){
+    $.post(ENDPOINT + "postCards.php", { "userid": userid, "cards": cards })
+    .done(function(data) {
+        jsonData = JSON.parse(data);
+        callback(jsonData);
+    })
+    .fail(function() { console.log("postCards Error"); })
+}
