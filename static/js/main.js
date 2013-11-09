526968217cc78750a6eb0f2f5ad7d5f72ebc2bea
Mark Miro <contact@markmiro.com>
2013-11-09 02:17:51 -0800

    Got gesture swiping working

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,18 @@
-(function() {
-    $('#slides').superslides();
-    $('#slides').bind("init.slides", function () {
-        $('#slides').superslides('animate', 1);
+$(function() {
+    var $slides = $('#slides');
+
+    Hammer($slides[0]).on("swipeleft", function(e) {
+        $slides.data('superslides').animate('next');
     });
-}());
+
+    Hammer($slides[0]).on("swiperight", function(e) {
+        $slides.data('superslides').animate('prev');
+    });
+
+    $slides.superslides({
+        hashchange: true
+    });
+    $slides.bind("init.slides", function () {
+        $slides.superslides('animate', 1);
+    });
+});
